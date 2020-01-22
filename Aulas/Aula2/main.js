19e2ed8b3e362c4fec515ad761de9866b1eb1ce7
unknown <brunotestaguzza@gmail.com>
2020-01-22 16:25:26 -0300

    Estamos adicionando mais coisas

--- Aulas/Aula2/main.js
+++ Aulas/Aula2/main.js
@@ -5,6 +5,84 @@ const express = require("express")
 //     console.log("Testando for mais uma vez");    
 // }
 
+// Objetos
+let pet = [{
+    "nome": "Batman",
+    "raca": "poodle",
+    "genero": "feminino",
+    "idade": 3,
+    "tipo": "cao",
+    "vacinado": true,
+    "servicos": ["banho", "tosa", "corte"]
+},
+{
+    "nome": "Tesla",
+    "raca": "poodle",
+    "idade": 3,
+    "genero": "masculino",
+    "tipo": "gato",
+    "vacinado": false,
+    "servicos": ["banho", "tosa", "corte"]
+},
+{
+    "nome": "Pokemon",
+    "raca": "chiwawa",
+    "idade": 20,
+    "genero": "nao definido",
+    "tipo": "cao",
+    "vacinado": false,
+    "servicos": ["banho", "tosa", "corte"]
+},
+{
+    "nome": "Eagle",
+    "raca": "hotwaile",
+    "idade": 5,
+    "genero": "masculino",
+    "tipo": "cao",
+    "vacinado": true,
+    "servicos": ["banho", "tosa", "corte"]
+},
+]
+
+function dataNacimento(obj){
+    return 2020 - obj.idade;
+}
+
+(function Imprimi(obj){
+    console.log("Projeto Petshop");
+    console.log("------------------------------------------")
+    obj.forEach(element => {
+        console.log("nome:" + element.nome);
+        console.log("raca:" + element.raca);
+        console.log("idade:" + element.idade);
+        console.log("genero:" + ((element.genero==='masculino')?"M":"F"));
+        console.log("tipo:" + element.tipo);
+        console.log("vacinado:" + ((element.vacinado===true)?"Sim":"Não"));
+        console.log("servicos:" + element.servicos);
+        console.log("------------------------------------------")
+    });
+})(pet)
+
+function Vacina(obj){
+    if (!obj.vacinado){
+        obj.vacinado = true;
+        console.log("Pet vacinado com sucesso")
+    }else{
+        console.log("Pet ja esta vacinado")        
+    }
+
+    //return obj;
+}
+
+// pet.forEach(element => {
+//     Vacina(element)
+//     console.log(element)
+// });
+
+// pet.forEach(element => {
+//     Vacina(element)
+//     console.log(element)
+// });
 
 class Pet {
     constructor(nome, raca, genero, tipo ,vacinado, servicos){
@@ -19,49 +97,10 @@ class Pet {
     get Nome(){
         return this.nome;
     }
+
 }
 
 let Julia = new Pet("Julia", "poodle", "feminino", "seila", true, ["banho", "tosa", "corte"]);
-// Objetos
-let pet = [{
-    "nome": "Batman",
-    "raca": "poodle",
-    "genero": "feminino",
-    "tipo": "",
-    "vacinado": true,
-    "servicos": ["banho", "tosa", "corte"]
-},
-{
-    "nome": "Tesla",
-    "raca": "poodle",
-    "genero": "masculino",
-    "tipo": "",
-    "vacinado": true,
-    "servicos": ["banho", "tosa", "corte"]
-},
-{
-    "nome": "Pokemon",
-    "raca": "chiwawa",
-    "genero": "undefined",
-    "tipo": "",
-    "vacinado": false,
-    "servicos": ["banho", "tosa", "corte"]
-},
-{
-    "nome": "Eagle",
-    "raca": "hotwaile",
-    "genero": "masculino",
-    "tipo": "",
-    "vacinado": true,
-    "servicos": ["banho", "tosa", "corte"]
-},
-]
-pet.forEach(element => {
-    console.log(element.nome)
-});
-
-console.log(Julia.Nome);
-
 const app = express();
 
 app.get('/', function (req, res) {
